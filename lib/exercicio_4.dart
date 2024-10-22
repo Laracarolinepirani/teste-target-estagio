7801b8cb7ebcb20f3be40044eadaaf2a1b0c29c7
LARA CAROLINE PIRANI OLIVEIRA <laracaroline.p@hotmail.com>
2024-10-22 16:40:53 -0300

    exercicio 4

--- lib/exercicio_4.dart
+++ lib/exercicio_4.dart
@@ -3,21 +3,19 @@ void elemeto() {
   Descubra a lógica e complete o próximo elemento:
 
   a) 1, 3, 5, 7, ___
-  R: É uma sequencia de números impares, sendo o próximo número 9
+  R: sequência de números ímpares, o próximo número 9
 
   b) 2, 4, 8, 16, 32, 64, ____
-  R: É uma sequencia de números que é o dobro do número anterior, então o próximo será 128
+  R: duplicando o número anterior, o próximo será 128
 
   c) 0, 1, 4, 9, 16, 25, 36, ____
-  R: é uma sequencia que são o valor ao quadrado da sequencia dele, como 0^2 = 0, 1^2 = 1, 2^2 = 4, 3^2 = 9...,
-    então por fim no ultimo valor é 7^2 = 49
+  R: quadrado dos números inteiros, o próximo será49
 
   d) 4, 16, 36, 64, ____
-  R: Da mesma maneira que a sequência anterior, porém apenas com números pares  2^2 = 4, 4^2 = 16, 6^2 = 36, 8^2 = 64...
-    então por fim no ultimo valor é 10^2 = 100
+  R: quadrado de múltiplos de 2 (2², 4², 6², 8², ...), o próximo será= 100
 
   e) 1, 1, 2, 3, 5, 8, ____
-  R: Aqui o número é a soma dos dois anteriores, então na sequência seria 5 + 8 = 13.
+  R: sequência de Fibonacci, o próximo será 13.
 
   f) 2,10, 12, 16, 17, 18, 19, ____
   R: Essa sequência não segue uma progressão aritmética ou geométrica clara, Com base nesse padrão de pequenos incrementos no final, é provável que o próximo número da sequência seja 20, continuando a aumentar em pequenos incrementos
