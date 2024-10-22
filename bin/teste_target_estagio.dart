ade84d6c18adcbabcd7094c8133d0102039ab5f4
LARA CAROLINE PIRANI OLIVEIRA <laracaroline.p@hotmail.com>
2024-10-22 14:22:04 -0300

    exercicio 3 finalizado

---
+++ bin/teste_target_estagio.dart
@@ -0,0 +1,5 @@
+import 'package:teste_target_estagio/teste_target_estagio.dart' as exercicio3;
+
+void main(List<String> arguments) {
+  exercicio3.calculate();
+}
---
+++ lib/teste_target_estagio.dart
@@ -0,0 +1,10 @@
+void calculate() {
+  int indice = 12;
+  int soma = 0;
+  int k = 1;
+  while (k < indice) {
+    k = k + 1;
+    soma = soma + k;
+  }
+  print("o resultado é: $soma");
+}
